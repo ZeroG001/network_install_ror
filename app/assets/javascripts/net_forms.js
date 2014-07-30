0148fa269c852fa2cf06c9a5f3c49747339466c9
Blayne Holland <ZeroG001@hotmail.com>
2014-07-29 22:09:41 -0400

    Giving the project a little bit of a mobile feel. By that I mean, adding a more responsive layout

--- app/assets/javascripts/net_forms.js
+++ app/assets/javascripts/net_forms.js
@@ -2,6 +2,17 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 */
+$("document").ready(function(){
+
+	$sidebar = $('.sidebar-button');
+
+	$('.sidebar-button').click(function(){
+		
+		$(".sidebar").toggleClass("hidden-xs");
+	});
+
+
+});
 
 
     
