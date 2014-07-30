e1c44aa8ab03fb4bc2da831f02a445d53019d8e6
Blayne <blayneholland@outlook.com>
2014-07-30 17:02:41 -0400

    Working on off canvas bar. Hope to finish it by tonight

--- app/assets/javascripts/net_forms.js
+++ app/assets/javascripts/net_forms.js
@@ -4,15 +4,49 @@
 */
 $("document").ready(function(){
 
-	$sidebar = $('.sidebar-button');
+	$sidebar_button = $('.sidebar-button');
+  $sidebar = $(".sidebar");
 
-	$('.sidebar-button').click(function(){
+	$sidebar_button.click(function(){
 		
-		$(".sidebar").toggleClass("hidden-xs");
-	});
+		// $sidebar.toggleClass(function(){
+  //     if($(this).hasClass("hidden-xs")) {
+  //       return "hidden-xs"
+  //     } else {
+  //       setTimeout(function(){
+  //         return "hidden-xs"
+  //       },1000);
+  //     }
+      
+  //   });
+
+    if($sidebar.hasClass("sidebarSlideOut")) {
+      $sidebar.addClass("sidebarSlideIn");
+      setTimeout(function(){
+        $sidebar.addClass("hidden-xs");
+        $sidebar.removeClass("sidebarSlideOut");
+        $sidebar.removeClass("sidebarSlideIn");
+      },1000);
+    } else {
+      $sidebar.removeClass("sidebarSlideIn");
+      $sidebar.removeClass("hidden-xs");
+      $sidebar.addClass("sidebarSlideOut");
+    }
 
 
-});
+    // $sidebar.toggleClass(function(){
+    //   if ($sidebar.hasClass("hidden-xs")) {
+    //     return "sidebarSlideIn"
+    //   } else {
+    //     return "sidebarSlideOut"
+    //   }
+
+    // }); 
+
+	}); // Click function end
+
+
+}); // Document function end
 
 
     
