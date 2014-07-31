c787e7b558df760423d0c4551a99f4928da0c4e0
Blayne Holland <ZeroG001@hotmail.com>
2014-07-31 00:40:18 -0400

    I believe I have the sidebar working as I want it to. For the most part...

--- app/assets/javascripts/net_forms.js
+++ app/assets/javascripts/net_forms.js
@@ -4,44 +4,28 @@
 */
 $("document").ready(function(){
 
+//SideBar functionality. Like most things. this can be refactored.
 	$sidebar_button = $('.sidebar-button');
   $sidebar = $(".sidebar");
-
 	$sidebar_button.click(function(){
-		
-		// $sidebar.toggleClass(function(){
-  //     if($(this).hasClass("hidden-xs")) {
-  //       return "hidden-xs"
-  //     } else {
-  //       setTimeout(function(){
-  //         return "hidden-xs"
-  //       },1000);
-  //     }
-      
-  //   });
+   
+   if($sidebar.hasClass("sidebarSlideIn") || $sidebar.hasClass("sidebarSlideIn")) {
+    return false;
+   }
 
-    if($sidebar.hasClass("sidebarSlideOut")) {
-      $sidebar.addClass("sidebarSlideIn");
-      setTimeout(function(){
-        $sidebar.addClass("hidden-xs");
-        $sidebar.removeClass("sidebarSlideOut");
-        $sidebar.removeClass("sidebarSlideIn");
-      },1000);
-    } else {
+   if($sidebar.hasClass("hidden-xs")) {
+    $sidebar.removeClass("hidden-xs");
+    $sidebar.addClass("sidebarSlideOut");
+    setTimeout(function(){
+      $sidebar.removeClass("sidebarSlideOut");
+    },500); 
+   } else if (!$sidebar.hasClass("hidden-xs") && !$sidebar.hasClass("sidebarSlideOut") ) {
+    $sidebar.addClass("sidebarSlideIn")
+    setTimeout(function(){
+      $sidebar.addClass("hidden-xs");
       $sidebar.removeClass("sidebarSlideIn");
-      $sidebar.removeClass("hidden-xs");
-      $sidebar.addClass("sidebarSlideOut");
-    }
-
-
-    // $sidebar.toggleClass(function(){
-    //   if ($sidebar.hasClass("hidden-xs")) {
-    //     return "sidebarSlideIn"
-    //   } else {
-    //     return "sidebarSlideOut"
-    //   }
-
-    // }); 
+    },500);
+   } 
 
 	}); // Click function end
 
