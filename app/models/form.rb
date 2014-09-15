60a16691e1a49ed65844f154252c2464f672716e
Blayne Holland <ZeroG001@hotmail.com>
2014-09-14 21:52:42 -0400

    Added validations. Fixed small problem with date field in form

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,8 +1,17 @@
 class Form < ActiveRecord::Base	
 
+	# =============================
+	# => Database Associations
+	# =============================
+
 	belongs_to :user
 
+	# =============================
+	# => Before Filters/Actions
+	# =============================
+
 	before_save :change_date
+
 	# Change date expire data and complete date for form that have expired.
 	# If there is not expire date and complete date then add one.
 	def change_date
@@ -11,13 +20,13 @@ class Form < ActiveRecord::Base
 
 		if (self.completed == "1" and self.complete_date.nil? and self.expire_date.nil?)
 			self.complete_date = Chronic.parse("today at 12:00am")
-			self.expire_date = Chronic.parse("tomorrow at 12:00am") #This needs to be changed to next year
+			self.expire_date = Chronic.parse("a year from now") # Expiredate entered here
 		end 
 
 		# If the form has expired and is complete then renew.
 		if self.completed == "1" and Chronic.parse("today") > self.expire_date
 		self.complete_date = Chronic.parse("today at 12:00am")
-		self.expire_date = Chronic.parse("tomorrow at 12:00am")	#This needs to be changed to next year
+		self.expire_date = Chronic.parse("next year from now") # Expire date entered here
 		end
 	end
 
@@ -29,18 +38,20 @@ class Form < ActiveRecord::Base
       	end
 	end
 
-	
-	# validates :first_name, presence: :true
-	# validates :last_name, presence: :true
-	# validates :paynum, 
-	#			presence: :true,
-	#			with: {format: /[0-9]+/},
-	#			length: {maximum: 6}
-	# validates :service_cost, presence: :true
-	# validates :nic_connection, presence: :true
-	# validates :os_type, presence: :true
-	# validates :phone_number, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/ , message: "use the correct format"}
-	# validates :printer_ip, format: { with: /\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/, message: "IP address not valid" }
+	# =============================
+	# => Validations
+	# =============================
+
+	validates :first_name, presence: :true
+	validates :last_name, presence: :true
+	validates :paynum, 
+				presence: :true,
+				length: {maximum: 6},
+				format: {with: /[0-9]+/, message: "Pay number must contain only numbers"}
+	validates :service_cost, presence: :true
+	validates :nic_connection, presence: :true
+	validates :phone_number, allow_blank: :true, format: { with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/, message: "use the correct format"}
+	validates :printer_ip, allow_blank: :true, format: { with: /\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/, message: "IP address not valid" }
  
 
 	serialize :printer_ip, Array
